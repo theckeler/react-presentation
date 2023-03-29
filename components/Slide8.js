549b1b51d4000c15538cfc455a3a9d1526dc9adb
Todd Heckeler <todd@consumedesign.com>
2023-03-29 14:04:39 -0400

    fix

--- components/Slide8.js
+++ components/Slide8.js
@@ -7,7 +7,7 @@ export default function Slide7({ slideContainerCSS }) {
 					This slideshow was created using React.js, one of the most popular
 					front-end frameworks for building web applications. We hope you found
 					this presentation informative and helpful. If you have any questions
-					or comments, please don't hesitate to reach out.
+					or comments, please don&apos;t hesitate to reach out.
 				</p>
 			</div>
 		</>
